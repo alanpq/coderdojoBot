b03f2e6ddc7d591393c70ceb2eac1d76e5eb1e2a
Alan Panayotov <alanpanayotov@gmail.com>
2017-06-05 14:50:37 +0100

    Modularised bot
    Added easy command and event registration, and swear replacement

--- bot.js
+++ bot.js
@@ -1,16 +1,135 @@
 'use strict';
+
+const fs = require("fs");
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 
 var args = process.argv.slice(2);
 const token = args[0];
 
 var roles = {};
 
+//////////////////////////////////////////begin modularisation///////////////////////////////////////////////
+
+const config = require("./config.json");
+
+var _channel; //global channel (since this bot is only for the CoderDojo channel)
+var log, warn, error;
+
+//reroute console.log, warn and error to also send to channels
+const _log = console.log;
+const _warn = console.warn;
+const _error = console.error;
+
+console.log = function(options, msg) {
+  if(typeof options == "object")
+    Array.prototype.shift.call(arguments);
+  else
+    msg = options;
+  
+  _log.apply(this, arguments);
+  if(!log) return;
+  log.send({embed: new Discord.RichEmbed()
+    .setTitle('Log')
+    .setAuthor('CoderDojo Bot', client.user.avatarURL)
+    .setColor(0x00AE86)
+    .setDescription(msg)
+    //adds thumbnail if there is a user in question
+    .setThumbnail((options.user) ? options.user.avatarURL : "")
+    .setTimestamp()
+  });
+}
+
+console.warn = function(options, msg) {
+  if(typeof options == "object")
+    Array.prototype.shift.call(arguments);
+  else
+    msg = options;
+  
+  _warn.apply(this, arguments);
+  if(!warn) return;
+  warn.send({embed: new Discord.RichEmbed()
+    .setTitle('Warning')
+    .setAuthor('CoderDojo Bot', client.user.avatarURL)
+    .setColor(0xf1c40f)
+    .setDescription(msg)
+    //adds thumbnail if there is a user in question
+    .setThumbnail((options.user) ? options.user.avatarURL : "")
+    .setTimestamp()
+  });
+}
+
+console.error = function(options, msg) {
+  if(typeof options == "object")
+    Array.prototype.shift.call(arguments);
+  else
+    msg = options;
+  
+  _error.apply(this, arguments);
+  if(!warn) return;
+  error.send({embed: new Discord.RichEmbed()
+    .setTitle('Error')
+    .setAuthor('CoderDojo Bot', client.user.avatarURL)
+    .setColor(0xe74c3c)
+    .setDescription(msg)
+    //adds thumbnail if there is a user in question
+    .setThumbnail((options.user) ? options.user.avatarURL : "")
+    .setTimestamp()
+  });
+}
+
+
+//attach client events to external js files
+//based on the name of the file, it is attached to different events
+//eg. message.js -> client.on('message')
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunction = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    
+    client.on(eventName, (...args) => eventFunction.run(client, config, ...args));
+  });
+});
+
+//dynamic command system by attempting to load js file matching client message
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(config.cmdPrefix)) return;
+
+  const args = message.content.split(" ");
+  const command = args.shift().slice(config.cmdPrefix.length);
+  
+  //SANITISE!
+  if(/[(\/)(\.)]/.test(command)) {
+    console.warn({user: message.author}, message.author + " (" + message.author.username + ") tried to load js outside of commands folder!")
+    message.channel.send(message.author + ", naughty!");
+    message.delete();
+    return;
+  }
+  
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+
+//////////////////////////////////////////////back to icky code//////////////////////////////////////////////
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
   roles.ninja = client.guilds.first().roles.find('name', 'Ninjas');
   roles.over13 = client.guilds.first().roles.find('name', 'Over 13');
+  
+  _channel = client.guilds.first();
+  log = _channel.channels.find('name', config.logChannel);
+  warn = _channel.channels.find('name', config.warnChannel);
+  error = _channel.channels.find('name', config.errorChannel);
 });
 
 client.on('message', msg => {
@@ -28,7 +147,7 @@ client.on('message', msg => {
           pl.channel.send("Alright!");
           pl.member.addRole(roles.ninja);
         } else {
-          pl.channel.send("That wasn't a valid response.\nPlease answer with \"yes\" or \"no\"");
+          pl.channel.send('That wasn\'t a valid response.\nPlease answer with "yes" or "no"');
           next = false;
           //pl.channel.send("");
         }
---
+++ events/message.swears.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+exports.run = (client, config, msg) => {
+  if(!config.pgChannels.includes(msg.channel.name)) return;
+  
+  var swears = Object.keys(config.swears);
+  var final = msg.content;
+  for(var i = 0; i < swears.length; i++) {
+    final = final.replace(new RegExp(swears[i], "g"), config.swears[swears[i]]);
+  }
+  
+  if(final != msg.content) {
+    msg.channel.send({embed: new Discord.RichEmbed()
+      .setAuthor(msg.author.username, msg.author.avatarURL)
+      .setColor(0xe74c3c)
+      .setDescription(final)
+      .setTimestamp(Date.parse(msg.timestamp))
+    });
+    msg.delete();
+  }
+}
